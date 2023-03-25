4eb309d46a19d953362ad7c78e0449402654f3d2
Roman Njoroge <roman.njoroge@njuguna.com>
2023-03-25 21:01:01 +0300

    feat: Error Handling

--- C#Tutorial/C#Tutorial/Program.cs
+++ C#Tutorial/C#Tutorial/Program.cs
@@ -5,7 +5,18 @@ namespace CTutorial {
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello my name is {0}", name);
+
+            try
+            {
+                string readNumber = Console.ReadLine();
+                int convertedString = Convert.ToInt32(readNumber);
+                Console.WriteLine("The number was able to be converted");
+            } catch (Exception)
+            {
+                Console.WriteLine("The number was not able to be converted");
+            }
         }
     }  
 }
