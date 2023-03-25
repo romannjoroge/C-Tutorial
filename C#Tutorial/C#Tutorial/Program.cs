a03a21219fd0810e0797bd8f1cadd91b72b0702a
Roman Njoroge <roman.njoroge@njuguna.com>
2023-03-25 17:01:00 +0300

    feat: Implementing simple C# app

---
+++ C#Tutorial/C#Tutorial/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CTutorial {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }  
+}
